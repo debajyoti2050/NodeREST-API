ea695f5a9516261505df95651f38014343f7d558
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-05-06 03:51:51 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 const router=require("express").Router();
+const User=require("../models/User");
+const bcrypt=require("bcrypt");
 
 
-router.get("/",(req,res)=>{
-    res.send("hey its user route");
+// router.get("/",(req,res)=>{
+//     res.send("hey its user route");
+// })
+
+
+// updateuser
+router.put("/:id",async(req,res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+       if(req.body.password){
+           try{
+               const salt=await bcrypt.genSalt(10);
+               req.body.password=await bcrypt.hash(req.body.password,salt);
+           }catch(err){
+               return res.status(500).json(err)
+           }
+       }
+
+       try{
+           const user=await User.findByIdAndUpdate(req.params.id,{
+               $set: req.body,
+           });
+           res.status(200).json("account has been updated")
+       }catch(err){
+        return res.status(500).json(err)
+       }
+
+
+    }else{
+        return res.status(403).json("You can update your account only!!")
+    }
 })
+// deleteuser
+// get-a-User
+// follow-a-User
+// unfollow-a-user
 
 
 module.exports = router
